ba3099cefec5e73fde3f2cb83d5b85febc70b8d4
Tommy <contact@tommytran.io>
2025-10-28 01:17:29 -0700

    Update to Authentik 2025.10.0

--- policies/metropolis-is-not-banned.py
+++ policies/metropolis-is-not-banned.py
@@ -1,4 +1,4 @@
-if not ak_is_group_member(request.context["pending_user"], name="metropolis-banned"):
+if not ak_is_group_member(request.context["pending_user"], name="metropolis_banned"):
   return True
 ak_message("Account banned.")
 return False
