490380fa27647caf92afda814b43a4cb09d0ff4b
Tommy <contact@tommytran.io>
2025-12-08 03:03:44 -0700

    Create opencloud.py
    Signed-off-by: Tommy <contact@tommytran.io>

---
+++ property-mappings/opencloud.py
@@ -0,0 +1,17 @@
+# Scope: profile
+
+# Note that groups and roles are treated differently in OpenCloud
+if user.is_superuser:
+  role = "admin"
+else:
+  role = "user"
+
+if request.user.name == "":
+  request.user.name = request.user.username
+
+return {
+  "name": request.user.name,
+  "preferred_username": request.user.username,
+  "groups": [group.name for group in user.ak_groups.all()],
+  "role": role
+}
