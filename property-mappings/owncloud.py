d1e3877848409e415d26118e65761302e030552a
Tommy <contact@tommytran.io>
2025-12-08 00:02:11 -0700

    Cleaner oCIS property mappings
    Signed-off-by: Tommy <contact@tommytran.io>

--- property-mappings/owncloud.py
+++ property-mappings/owncloud.py
@@ -1,8 +1,5 @@
 # Scope: profile
 
-# Extract all groups the user is a member of
-groups = [group.name for group in user.ak_groups.all()]
-
 # Note that groups and roles are treated differently in oCIS
 if user.is_superuser:
   role = "admin"
@@ -13,8 +10,8 @@ if request.user.name == "":
   request.user.name = request.user.username
 
 return {
-  "username": request.user.username,
   "name": request.user.name,
-  "groups": groups,
+  "preferred_username": request.user.username,
+  "groups": [group.name for group in user.ak_groups.all()],
   "role": role
 }
