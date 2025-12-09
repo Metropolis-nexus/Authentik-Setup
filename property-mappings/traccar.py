8f8894281a7854012f344b9e0fede0feb845a59f
Tommy <contact@tommytran.io>
2025-12-08 17:14:38 -0700

    Styling improvement
    Signed-off-by: Tommy <contact@tommytran.io>

--- property-mappings/traccar.py
+++ property-mappings/traccar.py
@@ -1,13 +1,10 @@
 # Scope: profile
 
-# Extract all groups the user is a member of
-groups = [group.name for group in user.ak_groups.all()]
-
 if request.user.name == "":
   request.user.name = request.user.username
 
 return {
   "preferred_username": request.user.username,
   "name": request.user.name,
-  "groups": groups
+  "groups": [group.name for group in user.ak_groups.all()]
 }
