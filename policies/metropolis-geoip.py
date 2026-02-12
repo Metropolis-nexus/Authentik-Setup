83b3e3211bd6f8b9921dac99c9fb571af6791c50
Tommy <contact@tommytran.io>
2026-02-11 21:27:04 -0700

    Block Australian IP addresses
    Signed-off-by: Tommy <contact@tommytran.io>

--- policies/metropolis-geoip.py
+++ policies/metropolis-geoip.py
@@ -1,4 +1,7 @@
 if (context["geoip"]["country"] == "GB"):
   ak_message("United Kingdom IP addresses are not allowed.")
   return False
+elif (context["geoip"]["country"] == "AU"):
+  ak_message("Australian IP addresses are not allowed.")
+  return False
 return True
