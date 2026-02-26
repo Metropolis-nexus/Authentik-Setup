abf6f0950d5b33a78ec807b3399493dc5e073551
Tommy <contact@tommytran.io>
2026-02-25 21:31:21 -0700

    Remove AUS ban & allow 2FA with discoverable credentials

--- policies/metropolis-geoip.py
+++ policies/metropolis-geoip.py
@@ -1,7 +1,4 @@
 if (context["geoip"]["country"] == "GB"):
   ak_message("United Kingdom IP addresses are not allowed.")
   return False
-elif (context["geoip"]["country"] == "AU"):
-  ak_message("Australian IP addresses are not allowed.")
-  return False
 return True
