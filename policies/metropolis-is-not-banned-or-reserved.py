7966016ccca4f8b1b78e0b579c1fbde935002bbc
Tommy <contact@tommytran.io>
2026-02-27 19:17:40 -0700

    Update and rename metropolis-is-not-banned.py to metropolis-is-not-banned-or-reserved.py
    Signed-off-by: Tommy <contact@tommytran.io>

---
+++ policies/metropolis-is-not-banned-or-reserved.py
@@ -0,0 +1,7 @@
+if ak_is_group_member(request.context["pending_user"], name="metropolis_banned"):
+  ak_message("Account banned.")
+  return False
+elif ak_is_group_member(request.context["pending_user"], name="metropolis_reserved"):
+  ak_message("Account reserved. How did you even get this far?!")
+  return False
+return True
--- policies/metropolis-is-not-banned.py
+++
@@ -1,4 +0,0 @@
-if not ak_is_group_member(request.context["pending_user"], name="metropolis_banned"):
-  return True
-ak_message("Account banned.")
-return False
