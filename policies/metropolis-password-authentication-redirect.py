6bca9257be6f29005c8a752781bdfaf5780830c7
Tommy <contact@tommytran.io>
2026-02-27 22:11:29 -0700

    Combine metropolis-authentication-redirect and metropolis-password-authentiction-redirect

--- policies/metropolis-password-authentication-redirect.py
+++
@@ -1,3 +0,0 @@
-if request.context["pending_user"].attributes.get("passwordless_auth", "No") == "Yes":
-  return False
-return True
--- policies/metropolis-passwordless-authentication-redirect.py
+++ policies/metropolis-passwordless-authentication-redirect.py
@@ -1,3 +1,7 @@
-if request.context["pending_user"].attributes.get("standard_auth", "Yes") == "No" and ak_user_has_authenticator(context['pending_user'], 'webauthn'):
+if request.context["pending_user"].attributes.get("passwordless_auth", "No") == "No":
   return True
-return False
+
+if ak_user_has_authenticator(context['pending_user'], 'webauthn'):
+  return False
+  
+return True
